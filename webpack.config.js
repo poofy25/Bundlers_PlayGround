78f3ac5ecff632b70f5eadf7885dbc79359691e9
poofy25 <117314241+poofy25@users.noreply.github.com>
2023-01-29 15:38:53 +0200

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,14 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
      },
+     devServer: {
+      static:{
+         directory: path.resolve(__dirname, 'dist')
+      },
+      port: 3000,
+      open:true,
+      hot:true,
+      compress:true,
+      historyApiFallback: true,
+     }
 }
